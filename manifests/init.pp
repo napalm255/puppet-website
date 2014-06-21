71c355ae6d42a037083420a45a018ec4c2ffdefe
napalm255 <napalm255@gmail.com>
2014-06-21 18:59:16 -0400

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,68 @@
+class srv_website (
+
+  $wwwEnabled = true,
+  $wwwDirectory = '/opt/www',
+  $wwwOwner = 'nginx',
+  $wwwGroup = 'nginx',
+  $wwwMode = 755,
+
+  $jailEnabled = true,
+  $jailDirectory = '/opt/jail',
+  $jailOwner = 'root',
+  $jailGroup = 'root',
+  $jailMode = 754,
+
+  $ebsMountEnabled = false,
+  $ebsMountDirectory = '/data',
+  $ebsMountOwner = 'root',
+  $ebsMountGroup = 'root',
+  $ebsMountDevice = '/dev/xvdf1',
+  $ebsMountFstype = 'ext4',
+  $ebsMountOptions = 'defaults',
+  $ebsMountAtboot = true,
+
+) {
+
+  # www directory for websites
+  if $wwwEnabled == true {
+    file { "www_directory" :
+      name   => "www_directory",
+      ensure => "directory",
+      path   => $wwwDirectory,
+      owner  => $wwwOwner,
+      group  => $wwwGroup,
+      mode   => $wwwMode,
+    }
+  }
+
+  # jail directory for user home directories
+  if $jailEnabled == true {
+    file { "jail_directory" :
+      name   => "jail_directory",
+      ensure => "directory",
+      path   => $jailDirectory,
+      owner  => $jailOwner,
+      group  => $jailGroup,
+      mode   => $jailMode,
+    }
+  } 
+
+  # ebs mount volume for aws ec2
+  if $ebsMountEnabled == true {
+    file { $ebsMountDirectory :
+      ensure => "directory",
+      owner  => $ebsMountOwner,
+      group  => $ebsMountGroup,
+    }
+
+    mount { $ebsMountDirectory :
+      ensure  => "mounted",
+      device  => $ebsMountDevice,
+      fstype  => $ebsMountFstype,
+      options => $ebsMountOptions,
+      atboot  => $ebsMountAtboot,
+      require => File[$ebsMountDirectory],
+    }
+  }
+
+}
---
+++ manifests/website.pp
@@ -0,0 +1,161 @@
+# website definition
+define srv_website::website (
+  
+  $userName,
+  $userUid,
+  $groupGid,
+  $domainName,
+
+  $groupName = $userName,
+
+  $userGroups = [$groupName],
+  $userHomeRoot = '/opt/jail',
+  $userHome = "/opt/jail/${userName}",
+  $userPassword = '!',
+  $userPasswordMaxAge = '99999',
+  $userPasswordMinAge = '0',
+  $userShell = '/bin/bash',
+  $userManageHome = true,
+  $userEnsure = 'present',
+
+  $domainDirRoot = '/opt/www',
+  $domainDir = "/opt/www/${domainName}",
+  $domainDirMode = 2770,
+  $domainDirOwner = $userName,
+  $domainDirGroup = $groupName,
+  $domainDirSymlink = undef,
+
+  $gitRepoUrl = undef,
+  $gitRepoEnsure = 'latest',
+  $content_site = '',
+
+) {
+
+  # create user and group
+  user { "user_${userName}" :
+    ensure           => $userEnsure,
+    name             => $userName,
+    gid              => $groupGid,
+    groups           => ["${userName}"],
+    home             => $userHome,
+    password         => $userPassword,
+    password_max_age => $userPasswordMaxAge,
+    password_min_age => $userPasswordMinAge,
+    shell            => $userShell,
+    uid              => $userUid,
+    managehome       => $userManageHome,
+    require          => [ File[$userHomeRoot] ],
+  }
+  group { "group_${groupName}" :
+    ensure => $userEnsure,
+    name   => $groupName,
+    before => User["user_${userName}"],
+    gid    => $groupGid,
+  }
+
+  # create directory structure
+  file { "domain_dir_root_${domainName}" :
+    ensure  => 'directory',
+    path    => $domainDir,
+    owner   => $domainDirOwner,
+    group   => $domainDirGroup,
+    mode    => $domainDirMode,
+    require => [ User["user_${userName}"] ],
+  }
+
+  # http
+  if $domainDirSymlink == undef {
+    file { "domain_dir_http_${domainName}" :
+      ensure  => 'directory',
+      path    => "${domainDir}/http",
+      owner   => $domainDirOwner,
+      group   => $domainDirGroup,
+      mode    => $domainDirMode,
+      require => [ User["user_${userName}"], File["domain_dir_root_${domainName}"] ],
+    }
+    exec { "setfacl-${domainName}-http":
+      command => "/usr/bin/setfacl -m d:u::rwx -m d:g::rwx -m d:o::- ${domainDir}/http",
+      onlyif  => "/usr/bin/getfacl -cdp ${domainDir}/http | /bin/awk '/user::rwx/{a=1}/group::rwx/{b=1}/other::---/{c=1} END {exit (a+b+c==3) }'",
+      require => [ File["domain_dir_http_${domainName}"] ],
+    }
+  }
+  else {
+    file { "domain_dir_http_${domainName}" :
+      ensure  => 'link',
+      path    => "${domainDir}/http",
+      target  => $domainDirSymlink,
+      require => [ User["user_${userName}"], File["domain_dir_root_${domainName}"] ],
+    }
+  }
+
+  # logs
+  file { "domain_dir_logs_${domainName}" :
+    ensure  => 'directory',
+    path    => "${domainDir}/logs",
+    owner   => 'nginx',
+    group   => $groupName,
+    mode    => $domainDirMode,
+    require => [ User["user_${userName}"], File["domain_dir_root_${domainName}"] ],
+  }
+
+  exec { "setfacl-${domainName}-logs":
+    command => "/usr/bin/setfacl -m d:u::rw -m d:g::rw -m d:o::- ${domainDir}/logs",
+    onlyif  => "/usr/bin/getfacl -cdp ${domainDir}/logs | /bin/awk '/user::rw-/{a=1}/group::rw-/{b=1}/other::---/{c=1} END {exit (a+b+c==3) }'",
+    require => [ File["domain_dir_logs_${domainName}"] ],
+  }
+
+  # stats
+  file { "domain_dir_stats_${domainName}" :
+    ensure  => 'directory',
+    path    => "${domainDir}/stats",
+    owner   => $userName,
+    group   => $groupName,
+    mode    => $domainDirMode,
+    require => [ User["user_${userName}"], File["domain_dir_root_${domainName}"] ],
+  }
+
+  exec { "setfacl-${domainName}-stats":
+    command => "/usr/bin/setfacl -m d:u::rw -m d:g::rw -m d:o::- ${domainDir}/stats",
+    onlyif  => "/usr/bin/getfacl -cdp ${domainDir}/stats | /bin/awk '/user::rw-/{a=1}/group::rw-/{b=1}/other::---/{c=1} END {exit (a+b+c==3) }'",
+    require => [ File["domain_dir_stats_${domainName}"] ],
+  }
+
+  # create nginx config file
+  file { "/etc/nginx/conf.d/${userName}.conf":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0644,
+    content => template('srv_website/nginx.erb'),
+  }
+
+  # create php-fpm config file
+  file { "/etc/php-fpm.d/${userName}.conf":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0644,
+    content => template('srv_website/fpm.erb'),
+  }
+
+  # ensure fpm.sock is correct user/group of nginx
+  file { "fpm.sock_${domainName}" :
+    path    => "${domainDir}/fpm.sock",
+    ensure  => present,
+    owner   => 'nginx',
+    group   => 'nginx',
+    mode    => 0600,
+    require => [ File["domain_dir_root_${domainName}"] ],
+  }
+
+  if $gitRepoUrl != undef {
+    vcsrepo { "${domainDir}/http":
+      ensure   => $gitRepoEnsure,
+      provider => 'git',
+      source   => $gitRepoUrl,
+      owner    => $userName,
+      group    => $groupName,
+      require  => Exec["setfacl-${domainName}-http"],
+    }
+  }
+}
