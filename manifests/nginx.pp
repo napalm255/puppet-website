1ea35e8d0ea71a9361b009997cff93057b51835f
napalm255 <napalm255@gmail.com>
2014-06-22 21:45:13 -0400

    added nginx back for now

---
+++ manifests/nginx.pp
@@ -0,0 +1,24 @@
+class website::nginx () {
+
+  package { 'nginx':
+    ensure => latest,
+  }
+
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    require => Package['nginx'],
+  }
+
+  # create nginx config file
+  file { "/etc/nginx/nginx.conf":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0644,
+    content => template('srv_website/nginx.conf.erb'),
+    notify  => [ Service['nginx'] ],
+    require => Package['nginx'],
+  }
+
+}
